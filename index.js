29dbe0c89f8f17a0e3e5284eeac968e7cfcf5f11
SneakyTurtle <dejan.duric1@gmail.com>
2018-03-12 21:11:36 -0700

    E-card

---
+++ index.js
@@ -0,0 +1,187 @@
+document.getElementById("calander").addEventListener("click", function(){
+   document.getElementById("fact2").style.display = "block";
+    document.getElementById("egg6").style.display = "block";
+    document.getElementById("egg6").style.width = "450px";
+    document.getElementById("egg6").style.height = "400px";
+    document.getElementById("egg6").style.top = "100px";
+    document.getElementById("egg6").style.left = "450px";
+    document.getElementById("egg6").style.right = "0";
+    document.getElementById("egg6").style.bottom = "0";
+    document.getElementById("egg6").style.zIndex = "1";
+});
+
+document.getElementById("fact2").addEventListener("click", function(){
+   document.getElementById("fact2").style.display = "none"; 
+});
+
+document.getElementById("fact2").addEventListener("click", function(){
+   document.getElementById("egg6").style.display = "none"; 
+});
+
+
+document.getElementById("rabbit2").addEventListener("click", function(){
+   document.getElementById("fact1").style.display = "block";
+    document.getElementById("egg1").style.display = "block";
+    document.getElementById("egg1").style.width = "450px";
+    document.getElementById("egg1").style.height = "400px";
+    document.getElementById("egg1").style.top = "100px";
+    document.getElementById("egg1").style.left = "450px";
+    document.getElementById("egg1").style.right = "0";
+    document.getElementById("egg1").style.bottom = "0";
+    document.getElementById("egg1").style.zIndex = "1";
+    document.getElementById("HideenRabbit").style.display = "block";
+});
+
+document.getElementById("fact1").addEventListener("click", function(){
+   document.getElementById("fact1").style.display = "none";
+   document.getElementById("egg1").style.display = "none"; 
+});
+
+document.getElementById("basket").addEventListener("click", function(){
+   document.getElementById("fact7").style.display = "block";
+    document.getElementById("egg7").style.display = "block";
+    document.getElementById("egg7").style.width = "450px";
+    document.getElementById("egg7").style.height = "400px";
+    document.getElementById("egg7").style.top = "100px";
+    document.getElementById("egg7").style.left = "450px";
+    document.getElementById("egg7").style.right = "0";
+    document.getElementById("egg7").style.bottom = "0";
+    document.getElementById("egg7").style.zIndex = "1";
+});
+
+document.getElementById("fact7").addEventListener("click", function(){
+   document.getElementById("fact7").style.display = "none";
+    document.getElementById("egg7").style.display = "none";
+});
+
+document.getElementById("sun").addEventListener("click", function(){
+   document.getElementById("fact4").style.display = "block";
+    document.getElementById("egg5").style.display = "block";
+    document.getElementById("egg5").style.width = "450px";
+    document.getElementById("egg5").style.height = "400px";
+    document.getElementById("egg5").style.top = "100px";
+    document.getElementById("egg5").style.left = "450px";
+    document.getElementById("egg5").style.right = "0";
+    document.getElementById("egg5").style.bottom = "0";
+    document.getElementById("egg5").style.zIndex = "1";
+});
+
+document.getElementById("fact4").addEventListener("click", function(){
+   document.getElementById("fact4").style.display = "none";
+    document.getElementById("egg5").style.display = "none";
+});
+
+document.getElementById("rock").addEventListener("click", function(){
+   document.getElementById("fact8").style.display = "block";
+    document.getElementById("egg8").style.display = "block";
+    document.getElementById("egg8").style.width = "450px";
+    document.getElementById("egg8").style.height = "400px";
+    document.getElementById("egg8").style.top = "100px";
+    document.getElementById("egg8").style.left = "450px";
+    document.getElementById("egg8").style.right = "0";
+    document.getElementById("egg8").style.bottom = "0";
+    document.getElementById("egg8").style.zIndex = "1";
+});
+
+document.getElementById("fact8").addEventListener("click", function(){
+   document.getElementById("fact8").style.display = "none";
+    document.getElementById("egg8").style.display = "none";
+});
+
+document.getElementById("hole").addEventListener("click", function(){
+   document.getElementById("fact3").style.display = "block";
+    document.getElementById("egg3").style.display = "block";
+    document.getElementById("egg3").style.width = "450px";
+    document.getElementById("egg3").style.height = "400px";
+    document.getElementById("egg3").style.top = "100px";
+    document.getElementById("egg3").style.left = "450px";
+    document.getElementById("egg3").style.right = "0";
+    document.getElementById("egg3").style.bottom = "0";
+    document.getElementById("egg3").style.zIndex = "1";
+});
+
+document.getElementById("fact3").addEventListener("click", function(){
+   document.getElementById("fact3").style.display = "none";
+    document.getElementById("egg3").style.display = "none";
+});
+
+document.getElementById("clicktree").addEventListener("click", function(){
+   document.getElementById("fact10").style.display = "block";
+    document.getElementById("egg10").style.display = "block";
+    document.getElementById("egg10").style.width = "450px";
+    document.getElementById("egg10").style.height = "400px";
+    document.getElementById("egg10").style.top = "100px";
+    document.getElementById("egg10").style.left = "450px";
+    document.getElementById("egg10").style.right = "0";
+    document.getElementById("egg10").style.bottom = "0";
+    document.getElementById("egg10").style.zIndex = "1";
+});
+
+document.getElementById("fact10").addEventListener("click", function(){
+   document.getElementById("fact10").style.display = "none";
+    document.getElementById("egg10").style.display = "none";
+});
+
+
+
+document.getElementById("hole").addEventListener("click", function(){
+   document.getElementById("flower").style.display = "block";
+   document.getElementById("flower").style.zIndex = "0";
+});
+
+document.getElementById("fact3").addEventListener("click", function(){
+   document.getElementById("flower").style.display = "none"; 
+});
+
+document.getElementById("angel").addEventListener("click", function(){
+    document.getElementById("fact5").style.display = "block";
+    document.getElementById("egg4").style.display = "block";
+    document.getElementById("egg4").style.width = "450px";
+    document.getElementById("egg4").style.height = "400px";
+    document.getElementById("egg4").style.top = "100px";
+    document.getElementById("egg4").style.left = "450px";
+    document.getElementById("egg4").style.right = "0";
+    document.getElementById("egg4").style.bottom = "0";
+    document.getElementById("egg4").style.zIndex = "1";
+});
+
+document.getElementById("fact5").addEventListener("click", function(){
+   document.getElementById("fact5").style.display = "none";
+    document.getElementById("egg4").style.display = "none";
+});
+    
+ 
+document.getElementById("cloud").addEventListener("click", function(){
+    document.getElementById("fact9").style.display = "block";
+    document.getElementById("egg9").style.display = "block";
+    document.getElementById("egg9").style.width = "450px";
+    document.getElementById("egg9").style.height = "400px";
+    document.getElementById("egg9").style.top = "100px";
+    document.getElementById("egg9").style.left = "450px";
+    document.getElementById("egg9").style.right = "0";
+    document.getElementById("egg9").style.bottom = "0";
+    document.getElementById("egg9").style.zIndex = "1";
+});
+
+document.getElementById("fact9").addEventListener("click", function(){
+   document.getElementById("fact9").style.display = "none";
+    document.getElementById("egg9").style.display = "none";
+    document.getElementById("angel").animation.opacity = "0%";
+});
+
+document.getElementById("HideenRabbit").addEventListener("click", function(){
+    document.getElementById("fact6").style.display = "block";
+    document.getElementById("egg2").style.display = "block";
+    document.getElementById("egg2").style.width = "450px";
+    document.getElementById("egg2").style.height = "400px";
+    document.getElementById("egg2").style.top = "100px";
+    document.getElementById("egg2").style.left = "450px";
+    document.getElementById("egg2").style.right = "0";
+    document.getElementById("egg2").style.bottom = "0";
+    document.getElementById("egg2").style.zIndex = "1";
+});
+
+document.getElementById("fact6").addEventListener("click", function(){
+   document.getElementById("fact6").style.display = "none";
+    document.getElementById("egg2").style.display = "none";
+});
